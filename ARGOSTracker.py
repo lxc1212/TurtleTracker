782b3c6f5fc40df7e771a05525c06389493630cd
Xinchen Li <xl266@duke.edu>
2019-09-30 12:52:26 -0400

    loops through all data

--- ARGOSTracker.py
+++ ARGOSTracker.py
@@ -1,10 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Sep 30 12:07:48 2019
-
-@author: xl266
-"""
-
 #-------------------------------------------------------------
 # ARGOSTrackingTool.py
 #
@@ -13,20 +6,34 @@ Created on Mon Sep 30 12:07:48 2019
 # Created by: Xinchen Li (xl266@duke.edu)
 # Created on: Fall 2019
 #--------------------------------------------------------------
-# Copy and paste a line of data as the lineString variable value
-lineString = "20616	29051	7/3/2003 9:13	3	66	33.898	-77.958	27.369	-46.309	6	0	-126	529	3	401 651134.7	0"
 
-​# Use the split command to parse the items in lineString into a list object
-lineData = lineString
+# Create a variable pointing to the file with no header
+fileName = "data/raw/SaraNoHeader.txt"
 ​
-# Assign variables to specfic items in the list
-recordID = lineData[]              # ARGOS tracking record ID
-obsDateTime = lineData[]           # Observation date and time (combined)
-obsDate = obsDateTime.split()[]    # Observation date - first item in obsDateTime list object
-obsTime = obsDateTime.split()[]    # Observation time - second item in obsDateTime list object
-obsLC = lineData[]                 # Observation Location Class
-obsLat = lineData[]                # Observation Latitude
-obsLon = lineData[]                # Observation Longitude
+# Open the file as a read-only file object
+fileObj = open(fileName, 'r')
 ​
-# Print information to the use
-print ("Record {0} indicates Sara was seen at {1}N and {2}W on {3}".format(recordID, obsLat,obsLat,obsDate))
+# Read in all lines in the text file into a list variable
+lineList = fileObj.readlines()
+​
+# Closes the file object (now that we have all we need)
+fileObj.close()
+​
+# Extract the first line from the lineList
+for lineString in lineList:
+    
+    # Use the split command to parse the items in lineString into a list object
+    lineData = lineString.split("\t")
+​
+    # Assign variables to specfic items in the list
+    recordID = lineData[0]              # ARGOS tracking record ID
+    obsDateTime = lineData[2]           # Observation date and time (combined)
+    obsDate = obsDateTime.split()[0]    # Observation date - first item in obsDateTime list object
+    obsTime = obsDateTime.split()[1]    # Observation time - second item in obsDateTime list object
+    obsLC = lineData[3]                 # Observation Location Class
+    obsLat = lineData[5]                # Observation Latitude
+    obsLon = lineData[6]                # Observation Longitude
+​
+    # Print information to the user
+    print ("Record {0} indicates Sara was seen at {1}N and {2}W on {3}".
+           format(recordID,obsLat,obsLat,obsDate))
