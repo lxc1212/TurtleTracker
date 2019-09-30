78546d05707f404594fe605ee1ed18b897fda674
Xinchen Li <xl266@duke.edu>
2019-09-30 12:11:53 -0400

    Create ARGOSTracker.py
    "Initial commit of ARGOSTrackingTool script"

---
+++ ARGOSTracker.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 30 12:07:48 2019
+
+@author: xl266
+"""
+
+#-------------------------------------------------------------
+# ARGOSTrackingTool.py
+#
+# Description: Parses a line of ARGOS tracking data 
+#
+# Created by: Xinchen Li (xl266@duke.edu)
+# Created on: Fall 2019
+#--------------------------------------------------------------
